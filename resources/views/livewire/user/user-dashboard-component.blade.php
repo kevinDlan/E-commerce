7e46bfd662cc7d5ff3a9c58bd86b66903b0a90db
kevinDlan <kevinkone19@gmail.com>
2020-11-08 11:23:14 +0000

    projet settings

--- resources/views/livewire/user/user-dashboard-component.blade.php
+++ resources/views/livewire/user/user-dashboard-component.blade.php
@@ -1,5 +1,5 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    {{-- Knowing others is intelligence; knowing yourself is true wisdom !. --}}
 
     <h1>User/Customer Dashboard</h1>
 </div>
